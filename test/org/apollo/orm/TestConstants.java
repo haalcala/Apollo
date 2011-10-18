54c5a1f25a83c6fdd6d610382cae3467668360f0
Harold Alcala <harold.alcala@gmail.com>
2011-10-18 18:08:10 +0800

    .

--- test/org/apollo/orm/TestConstants.java
+++ test/org/apollo/orm/TestConstants.java
@@ -3,6 +3,7 @@ package org.apollo.orm;
 import java.util.Properties;
 
 public interface TestConstants {
+	
 	static class Util {
 		public static Properties getTestConf() throws Exception {
 			Properties ret = new Properties();
